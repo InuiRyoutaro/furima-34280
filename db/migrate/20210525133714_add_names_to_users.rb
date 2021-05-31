ec77f52bd9733007b5cd4261ba195924ed23344d
InuiRyoutaro <inuinuinui529@gmail.com>
2021-05-31 14:44:17 +0900

    新規登録・ログイン・ログアウト機能実装

---
+++ db/migrate/20210525133714_add_names_to_users.rb
@@ -0,0 +1,9 @@
+class AddNamesToUsers < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :famly_name, :string, null: false
+    add_column :users, :first_name, :string, null: false
+    add_column :users, :family_name_japanese, :string, null: false
+    add_column :users, :first_name_japanese, :string, null: false
+    add_column :users, :birth_day, :date, null: false
+  end
+end
