3fa1eff6c22d4c324afbd3fa76c17399a52cdc9e
InuiRyoutaro <inuinuinui529@gmail.com>
2021-06-17 01:15:18 +0900

    テストコードの実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :item do
-    
+    name {"Monster"}
+    price  {"1000"}
+    description {"ナイキ"}
+    prefecture_id {"1"}
+    category_id {"1"}
+    item_condition_id {"2"}
+    shipping_cost_id {"2"}
+    shipping_day_id {"2"}
+    user_id           {"1"}
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,86 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品出品機能' do
+
+    context "出品できる場合" do
+      it 'name,price,prefecture_id,category,description,item_condition,shipping_cost,shipping_day,user_idが存在すれば登録できる' do
+        expect(@item)
+      end
+    end
+
+    context "保存できない場合" do
+    it 'imageが空だと出品できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+    end
+
+      it 'nameが空では登録できないこと' do
+        @item.name = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+
+      it 'priceが空では登録できないこと' do
+        @item.price = nil
+        @item.valid?
+        expect(@item.errors.full_messages)
+      end
+
+      it 'priceが300円以下では登録できないこと' do
+        @item.price = '299'
+        @item.valid?
+        expect(@item.errors.full_messages)
+      end
+
+      it 'priceが全角数字は登録できない' do
+        @item.price = "１１１１"
+        @item.valid?
+      end
+
+      it 'prefectureが空では登録できないこと' do
+        @item.prefecture = nil
+        @item.valid?
+        expect(@item.errors.full_messages)
+      end
+
+      it 'categoryが空では登録できないこと' do
+        @item.category = nil
+        @item.valid?
+        expect(@item.errors.full_messages)
+      end
+
+      it 'descriptionが空では登録できないこと' do
+        @item.description = nil
+        @item.valid?
+        expect(@item.errors.full_messages)
+      end
+
+      it 'item_conditionが空では登録できないこと' do
+        @item.item_condition_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages)
+      end
+
+      it 'shipping_costが空では登録できないこと' do
+        @item.shipping_cost = nil
+        @item.valid?
+        expect(@item.errors.full_messages)
+      end
+
+      it 'shipping_dayが空では登録できないこと' do
+        @item.shipping_day = nil
+        @item.valid?
+        expect(@item.errors.full_messages)
+      end
+    end
+  end
 end
+
+
+
