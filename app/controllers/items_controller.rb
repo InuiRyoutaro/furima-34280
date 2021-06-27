94d1224c46289dc905fa5ece7940bce10745351f
InuiRyoutaro <inuinuinui529@gmail.com>
2021-06-27 12:56:14 +0900

    商品詳細編集ページ修正
    ・beforeアクションで呼び出す形に修正
    ・private以下にメソッド化
    ・もどるボタンを押すと詳細ページに遷移するように編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :abc, only: [:show, :edit, :update]
+  before_action :ensure_current_user, only: [:edit, :update]
+
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -19,18 +22,18 @@ class ItemsController < ApplicationController
   end
 
   def show  
-    @item = Item.find(params[:id])
+    #@item = Item.find(params[:id])
   end
 
   def edit  
-    @item = Item.find(params[:id])
-    unless current_user == @item.user
-      redirect_to root_path
-    end
+    #@item = Item.find(params[:id])
+    #unless current_user == @item.user
+      #redirect_to root_path
+    #end
   end
 
   def update  
-    @item = Item.find(params[:id])
+    #@item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item)
     else
@@ -41,6 +44,19 @@ class ItemsController < ApplicationController
   private
   def item_params
     params.require(:item).permit(:image, :name, :price, :prefecture_id, :category_id, :description, :item_condition_id, :shipping_cost_id, :shipping_day_id,:user_id,).merge(user_id: current_user.id)
+  end
+
+  def abc
+    @item = Item.find(params[:id])
+  end
+
+  def ensure_current_user  
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit
+    end
   end
 
 end
